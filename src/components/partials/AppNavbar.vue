738a5b0c1ba0165c5155b3cdaee462fad722b13c
Demscript <demscript@gmail.com>
2022-11-17 01:20:14 +0100

    fix

--- src/components/partials/AppNavbar.vue
+++ src/components/partials/AppNavbar.vue
@@ -1,341 +1,103 @@
 <template>
   <div>
+    <div id="preloder">
+    <div class="loader"></div>
+  </div>
 
-    <div
-      class="modal fade"
-      id="exampleModal"
-      tabindex="-1"
-      role="dialog"
-      aria-hidden="true"
-    >
-      <LoginUser />
+  <div class="offcanvas-menu-overlay"></div>
+  <div class="offcanvas-menu-wrapper">
+    <div class="offcanvas__option">
+      <div class="offcanvas__links">
+        <a href="#">Sign in</a>
+        <a href="#">FAQs</a>
+      </div>
+      <div class="offcanvas__top__hover">
+        <span>Usd <i class="arrow_carrot-down"></i></span>
+        <ul>
+          <li>USD</li>
+          <li>EUR</li>
+          <li>USD</li>
+        </ul>
+      </div>
     </div>
-    <!-- register -->
-    
-      <RegisterUser />
-    <!-- //modal -->
-    <!-- //top-header -->
+    <div class="offcanvas__nav__option">
+      <a href="#" class="search-switch"><img src="assets/img/icon/search.png" alt=""></a>
+      <a href="#"><img src="assets/img/icon/heart.png" alt=""></a>
+      <a href="#"><img src="assets/img/icon/cart.png" alt=""> <span>0</span></a>
+      <div class="price">$0.00</div>
+    </div>
+    <div id="mobile-menu-wrap"></div>
+    <div class="offcanvas__text">
+      <p>Free shipping, 30-day return or refund guarantee.</p>
+    </div>
+  </div>
 
-    <!-- header-bottom-->
-    <div class="header-bot" id="site-header">
-      <div class="container my-4">
-        <div
-          class="row header-bot_inner_wthreeinfo_header_mid align-items-center"
-        >
-          <!-- logo -->
-          <div class="col-lg-3 col-md-4 logo_agile">
-            <h1>
-              <a href="/"
-                ><img
-                  :src="`${publicPath}assets/images/logo.png`"
-                  class="img-fluid my-logo"
-              /></a>
-            </h1>
+
+  <header class="header">
+    <div class="header__top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-md-7">
+            <div class="header__top__left">
+              <p>Free shipping, 30-day return or refund guarantee.</p>
+            </div>
           </div>
-          <!-- //logo -->
-          <!-- header-bot -->
-          <div class="col-lg-9 col-md-8 header">
-            <div class="row align-items-center">
-              <!-- search -->
-              <div class="col-lg-8 col-sm-8 agileits_search">
-                <form
-                  class="form-inline"
-                  action="#"
-                  method="post"
-                  style="max-width: 600px"
-                >
-                  <input
-                    class="form-control"
-                    type="search"
-                    placeholder="Search for products, Categories and Sub categories"
-                    aria-label="Search"
-                    v-model="searchProducts.search"
-                    @keyup="searchProduct()"
-                    required
-                  />
-                  <button
-                    class="btn"
-                    type="submit"
-                    @click.prevent="searchProduct()"
-                  >
-                    <i class="fa fa-search" aria-hidden="true"></i>
-                  </button>
-                </form>
-                <div
-                  class="card search-area"
-                  v-if="products.data && searchProducts.search !== ''"
-                >
-                  <ul>
-                    <li v-for="(p, i) in products.data" class="pb-2" :key="i" @click="clearSearch()">
-                      <router-link :to="'/product/' + p.id">
-                        <p>
-                          {{ p.product_name }}
-                        </p>
-                      </router-link>
-                    </li>
-                  </ul>
-                </div>
+          <div class="col-lg-6 col-md-5">
+            <div class="header__top__right">
+              <div class="header__top__links">
+                <a href="#">Sign in</a>
+                <a href="#">FAQs</a>
               </div>
-              <!-- //search -->
-              <!-- cart details -->
-              <div
-                class="
-                  col-lg-2 col-sm-2
-                  top_nav_right
-                  text-center
-                  mt-sm-0 mt-2
-                  d-flex
-                  align-items-center
-                  justify-content-between
-                "
-              >
-                <div class="cart-store">
-                  <!-- toggle switch for light and dark theme -->
-                  <div class="cont-ser-position">
-                    <nav class="navigation">
-                      <div class="theme-switch-wrapper">
-                        <label class="theme-switch" for="checkbox">
-                          <input
-                            type="checkbox"
-                            @change="switchTheme($event)"
-                            id="checkbox"
-                          />
-                          <div class="mode-container">
-                            <i class="gg-sun"></i>
-                            <i class="gg-moon"></i>
-                          </div>
-                        </label>
-                      </div>
-                    </nav>
-                  </div>
-                  <!-- //toggle switch for light and dark theme -->
-                </div>
-                <div class="cart-store">
-                  <router-link to="/wishlist"><i class="far fa-heart"></i></router-link>
-                </div>
-                <div class="wthreecartaits wthreecartaits2 cart cart box_1">
-                  <router-link to="/cart"
-                      class="btn w3view-cart p-0"
-                      type="submit"
-                      name="submit"
-                      value=""
-                    >
-                      <i class="fa" style="font-size:24px">&#xf07a;</i>
-<span class='badge badge-warning' id='lblCartCount' v-if="carts.length>0"> {{carts.length}} </span>
-                    </router-link>
-                </div>
+              <div class="header__top__hover">
+                <span>Usd <i class="arrow_carrot-down"></i></span>
+                <ul>
+                  <li>USD</li>
+                  <li>EUR</li>
+                  <li>USD</li>
+                </ul>
               </div>
-              <div class="col-md-2 col-lg-2">
-                <!-- <button class="btn btn-sm btn-primary">
-                  Login/Register
-                </button> -->
-
-                <div class="nav-item" v-if="this.profile == null">
-                  <a class="btn btn-sm btn-default shadow-sm"
-                    href="#login"
-                    data-toggle="modal"
-                    data-target="#exampleModal"
-                    >Login/Register</a
-                  >
-                </div>
-                <div class="nav-item dropdown mr-lg-2 mb-lg-0 mb-2" v-else>
-                  <a
-                    class="button btn btn-sm btn-default shadow-sm dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    Hi {{ profile.name && profile.name.split(" ")[0] }}
-                  </a>
-                  <div class="dropdown-menu">
-                    <div class="agile_inner_drop_nav_info p-4">
-                      <!-- <h5 class="mb-3">Hi {{profile.name}}</h5> -->
-                      <div class="row ">
-                        <div class="col-md-12">
-                          <ul class="multi-column-dropdown">
-                            <li>
-                              <router-link to="/my-account">My Account</router-link>
-                            </li>
-                            <li>
-                              <a href="">Orders</a>
-                            </li>
-                            <!-- <hr /> -->
-                            <li>
-                              <div style="cursor:pointer;" @click.prevent="logout()"
-                                >Logout</div
-                              >
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- //cart details -->
             </div>
           </div>
         </div>
       </div>
     </div>
-    <!-- shop locator (popup) -->
-    <!-- //header-bottom -->
-    <!-- navigation -->
-    <NavBar2 v-if="this.$router.history.current.path != '/'" />
-    <!-- //navigation -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-3">
+          <div class="header__logo">
+            <a href="index.html"><img src="assets/img/logo.png" alt=""></a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6">
+          <nav class="header__menu mobile-menu">
+            <ul>
+              <li class="active"><a href="index.html">Home</a></li>
+              <li><a href="#">Product</a>
+                <ul class="dropdown">
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="shop-details.html">Shop Details</a></li>
+                  <li><a href="shopping-cart.html">Shopping Cart</a></li>
+                  <li><a href="checkout.html">Check Out</a></li>
+                  <li><a href="blog-details.html">Blog Details</a></li>
+                </ul>
+              </li>
+              <li><a href="blog.html">Company</a></li>
+              <li><a href="blog.html">Contact</a></li>
+              <li><a href="contact.html">Services</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div class="col-lg-3 col-md-3">
+          <div class="header__nav__option">
+            <a href="#" class="search-switch"><img src="assets/img/icon/search.png" alt=""></a>
+            <a href="#"><img src="assets/img/icon/heart.png" alt=""></a>
+            <a href="#"><img src="assets/img/icon/cart.png" alt=""> <span>0</span></a>
+            <div class="price">$0.00</div>
+          </div>
+        </div>
+      </div>
+      <div class="canvas__open"><i class="fa fa-bars"></i></div>
     </div>
+  </header>
+  </div>
 </template>
-<style>
-.search-area {
-  background: white;
-  position: absolute;
-  height: auto;
-  width: 93.5%;
-  z-index: 1000;
-  top: 200;
-  padding: 20px;
-  /* margin */
-}
-
-.search-area ul li {
-  list-style: none;
-  color: black !important;
-}
-.search-area ul li:hover {
-  background-color: whitesmoke !important;
-}
-
-</style>
-<script>
-import RegisterUser from "../../views/Auth/RegisterUser.vue";
-import LoginUser from "@/views/Auth/LoginUser.vue";
-import NavBar2 from "./AppNavbar2.vue"
-import { mapState } from "vuex";
-import { bus } from '../../main'
-export default {
-  components: {
-    RegisterUser,
-    LoginUser,
-    NavBar2
-  },
-  data() {
-    return {
-      publicPath: process.env.BASE_URL,
-      profile: {},
-      // categories: {},
-      filter: {
-        filters: "",
-      },
-      searchProducts: {
-        search: "",
-      },
-      products: {},
-      cat: "",
-      carts:{}
-    };
-  },
-  methods: {
-    themeSwitcher() {
-      const toggleSwitch = document.querySelector(
-        '.theme-switch input[type="checkbox"]'
-      );
-      const currentTheme = localStorage.getItem("theme");
-
-      if (currentTheme) {
-        document.documentElement.setAttribute("data-theme", currentTheme);
-
-        if (currentTheme === "dark") {
-          toggleSwitch.checked = true;
-        }
-      }
-    },
-    getCartItems() {
-      if(localStorage.getItem("2@39$*8")) {
-
-        this.$api
-          .get(this.dynamic_route(`/api/cart`))
-          .then((res) => {
-            this.carts = res.data.data;
-          })
-          .catch(() => {
-  
-          })
-          .finally(() => {
-  
-          });
-      }else{
-        if(localStorage.getItem("unique_id")) {
-          this.$api
-            .post(this.dynamic_route(`/api/cart/get`),{unique_id:localStorage.getItem("unique_id")})
-            .then((res) => {
-              this.carts = res.data.data;
-            })
-            .catch(() => {
-
-            })
-            .finally(() => {
-
-            });
-          }
-      }
-    },
-    switchTheme(e) {
-      if (e.target.checked) {
-        document.documentElement.setAttribute("data-theme", "dark");
-        localStorage.setItem("theme", "dark");
-      } else {
-        document.documentElement.setAttribute("data-theme", "light");
-        localStorage.setItem("theme", "light");
-      }
-    },
-    logout() {
-      localStorage.clear();
-      location.reload();
-    },
-    clearSearch() {
-      this.searchProducts.search = "";
-      this.products = {}
-    },
-    searchProduct() {
-      this.products = {};
-      // this.$api
-      //   .post(
-      //     this.dynamic_route(`/api/product/name`),
-      //     this.searchProducts
-      //   )
-      //   .then((res) => {
-      //     this.products = res.data.data;
-      //   })
-      //   .catch(() => {
-
-      //   });
-    },
-    getProdByCat(id) {
-      this.$router.push(`/products/${id}`);
-    },
-  },
-  mounted() {
-    this.profile = JSON.parse(localStorage.getItem("auth_user"));
-    this.getCartItems();
-    this.$store.dispatch("get_category", this.filter);
-  },
-  computed: {
-    ...mapState({
-      categories: (state) => state.category.categories,
-      items: (state) => state.product.items,
-    }),
-  },
-  created (){
-    bus.$on('updateCart', () => {
-      this.getCartItems();
-    })
-  }
-};
-</script>
-<style scoped>
-  .my-logo{
-    height: 40px !important;
-  }
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,6 @@
 import Vue from 'vue'
 import Home from '../views/Home/HomeIndex.vue'
 import VueRouter from 'vue-router'
-import Products from '../views/Products/ProductIndex.vue'
-import ProductDetails from '../views/Products/ProductDetails.vue'
-import Cart from '../views/Cart/CheckoutCart.vue'
-import WishList from '../views/Cart/WishList.vue'
-import PaymentPage from '../views/Payment/MakePayment.vue'
-import ViewProducts from '../views/Products/ViewProducts.vue'
-import Profile from '../views/Profile/MyAccount.vue'
-import CheckoutDetails from '../views/Cart/CheckoutDetails.vue'
-import InvoiceDetails from '../views/Cart/InvoiceDetails.vue'
-import InvoiceHistory from '../views/Cart/InvoiceHistory.vue'
 
 Vue.use(VueRouter)
 
@@ -21,61 +11,6 @@ const routes = [
     name: 'home-index',
     component: Home
   },
-  {
-    path: '/products',
-    name: 'home',
-    component: Products
-  },
-  {
-    path: '/product/:product_id',
-    name: 'product-details',
-    component: ProductDetails,
-  },
-  {
-    path: '/cart',
-    name: 'Cart',
-    component: Cart
-  },
-  {
-    path: '/pay-cart',
-    name: 'pay-cart',
-    component: PaymentPage
-  },
-  {
-    path: '/wishlist',
-    name: 'Wishlist',
-    component: WishList
-  },
-  {
-    path: '/products/:category_id?/:sub_category_id?/:nested_sub_category_id?/',
-    name: 'View Product',
-
-    component: ViewProducts
-  },
-
-  {
-    path: '/my-account',
-    name: 'Profile',
-    component: Profile
-  },
-
-  {
-    path: '/checkout-details',
-    name: 'Checkout Details',
-    component: CheckoutDetails
-  },
-
-  {
-    path: '/invoice/:invoice_id',
-    name: 'Invoice Details',
-    component: InvoiceDetails
-  },
-  {
-    path: '/my-invoices',
-    name: 'Invoice History',
-    component: InvoiceHistory
-  },
-
 ]
 
 const router = new VueRouter({
