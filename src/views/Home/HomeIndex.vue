58286d36ed414dd5a60601ac63a38a7d0d14d8ef
Demscript <demscript@gmail.com>
2022-12-22 12:20:38 +0100

    fix

--- src/views/Home/HomeIndex.vue
+++ src/views/Home/HomeIndex.vue
@@ -21,7 +21,7 @@
         </carousel>
       </div>
     </section>
-    <section class="best-choice py-5">
+    <!-- <section class="best-choice py-5">
       <div class="container">
         <div class="best-choice-content text-center">
           <h2>Our Best Choice</h2>
@@ -35,63 +35,8 @@
           </p>
         </div>
       </div>
-    </section>
+    </section> -->
 
-
-
-    <!-- ======= Our Services Section ======= -->
-    <section id="services" class="categories services sections-bg">
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-header text-center">
-          <h2>Support that we offer</h2>
-          <p>We are constantly developing our business to meet the ever-evolving needs of our customers.
-            Our goal is to work together, building trusted, long-term relationships, in order to create innovative
-            products and solutions.</p>
-        </div>
-
-        <div class="row gy-4" data-aos="fade-up" data-aos-delay="100">
-
-          <div class="col-lg-4 col-md-6">
-            <div class="service-item  position-relative">
-              <div class="icon">
-                <i class="bi bi-activity"></i>
-              </div>
-              <h3>Nesciunt Mete</h3>
-              <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis
-                tempore et consequatur.</p>
-              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
-            </div>
-          </div><!-- End Service Item -->
-
-          <div class="col-lg-4 col-md-6">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="bi bi-broadcast"></i>
-              </div>
-              <h3>Eosle Commodi</h3>
-              <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut
-                nesciunt dolorem.</p>
-              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
-            </div>
-          </div><!-- End Service Item -->
-
-          <div class="col-lg-4 col-md-6">
-            <div class="service-item position-relative">
-              <div class="icon">
-                <i class="bi bi-easel"></i>
-              </div>
-              <h3>Ledo Markt</h3>
-              <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci
-                eos earum corrupti.</p>
-              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
-            </div>
-          </div><!-- End Service Item -->
-
-        </div>
-
-      </div>
-    </section><!-- End Our Services Section -->
 
     <section class="product spad">
       <div class="container">
